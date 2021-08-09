2342e97c13d3147c8822769fbf1bbea087c2c528
Emre Can <emrecanyalcin1996@gmail.com>
2021-08-09 20:45:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>Related Digital</title>
+</head>
+
+<body>
+    <div class="container">
+        <br>
+    <div class="container">
+        <div class="row">
+            <form action="post.php" method="POST">
+                <div class="form-row">
+                    <h3>Auth Service Control</h3>
+                    <div class="col-md-4 mb-3">
+                        <label for="validationDefault01">Username</label>
+                        <input type="text" class="form-control" name="username" id="validationDefault01" placeholder="Username" required>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="validationDefault02">Password</label>
+                        <input type="password" class="form-control" name="password" id="validationDefault02" placeholder="Password" required>
+                    </div>
+                   
+                <button class="btn btn-primary" type="submit">Send</button>
+            </form>
+        </div>
+        </div>
+    </div>
+</body>
+
+</html>
