4ded71eb86e41a0b13c55ce58ebacf948775a920
Emre Can <emrecanyalcin1996@gmail.com>
2021-09-08 16:53:19 +0300

    Delete getApi.php

--- getApi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$apiName = $_POST['apiName'];
-if($api == null || $api == ""){
-    header("Location: index.php");
-}else{
-    require ("components/Form".$apiName."html");
-    echo $apiName;
-}
